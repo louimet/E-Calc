1d005dfb63edae6d609d7bf8cdab4075183b8b69
friketrike <friketrike@gmail.com>
2016-01-31 23:57:56 -0500

    Create Placeholder.java

---
+++ src/Placeholder.java
@@ -0,0 +1,5 @@
+public class Placeholder.java{
+    public static void main(String[] args){
+      System.out.println("Hello Team E, this is just while we get some code going on here...");
+    }
+}
