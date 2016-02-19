1a82ac94eccc9a7e8c50eec0c8dc70803af542a5
louimet <louimet@aei.ca>
2016-02-19 14:01:48 -0500

    Updated Log10.java
    - Added return statement for invalid input
    - Converted from main method to object method.

--- src/Log10.java
+++ src/Log10.java
@@ -20,37 +20,42 @@
  */
 
 public class Log10 {
-	// public double Log10(double number)
-	public static void main(String[] args) {
-		double number = 123456789.0;
-		double approx = 0;
-		double exponent = 0;
-		double newIncrement = 0.1;
-		double oldIncrement = 1;
-		double precision = 0.000001;	// Eventually, should be determined by the order of magnitude of the number entered
-		
-		for(; (number - approx) > 0; exponent = exponent + 1) {
-			approx = Math.pow(10, exponent);	// Replace by a call of Boulos's 10^x method
-			//System.out.println(exponent);
-		}
+	public double getLog10(double number) {
+	// public static void main(String[] args) {
+		// double number = 0.0;	// Remove in final version (number will be fed directly to method)
+		if(number <= 0) {
+			double badDouble = Double.NaN;
+			return badDouble;
+		} else {
+			double approx = 0;
+			double exponent = 0;
+			double newIncrement = 0.1;
+			double oldIncrement = 1;
+			double precision = 0.000001;	// Eventually, should be determined by the order of magnitude of the number entered
 			
-			//
-		while(Math.abs(number - approx) > precision) {	
-			for(; (number - approx) < oldIncrement; exponent = exponent - newIncrement) {
+			for(; (number - approx) > 0; exponent = exponent + 1) {
 				approx = Math.pow(10, exponent);	// Replace by a call of Boulos's 10^x method
 				//System.out.println(exponent);
-				//System.out.println((number - approx) + "+" + oldIncrement + " " + newIncrement);
 			}
-			for(; (number - approx) > newIncrement; exponent = exponent + newIncrement) {
-				approx = Math.pow(10, exponent);	// Replace by a call of Boulos's 10^x method
-				//System.out.println(exponent);
-				//System.out.println((number - approx) + "-" + newIncrement);
-			}
-			oldIncrement = newIncrement;
-			newIncrement = newIncrement / 10;
-		}				
-		// return exponent;
-		System.out.println(number + " equals 10 to the power of " + exponent + ".");	// Comment out in final version.
-		System.out.println("10^" + exponent + " = " + Math.pow(10, exponent));		// Comment out in final version.
+				
+				//
+			while(Math.abs(number - approx) > precision) {	
+				for(; (number - approx) < oldIncrement; exponent = exponent - newIncrement) {
+					approx = Math.pow(10, exponent);	// Replace by a call of Boulos's 10^x method
+					//System.out.println(exponent);
+					//System.out.println((number - approx) + "+" + oldIncrement + " " + newIncrement);
+				}
+				for(; (number - approx) > newIncrement; exponent = exponent + newIncrement) {
+					approx = Math.pow(10, exponent);	// Replace by a call of Boulos's 10^x method
+					//System.out.println(exponent);
+					//System.out.println((number - approx) + "-" + newIncrement);
+				}
+				oldIncrement = newIncrement;
+				newIncrement = newIncrement / 10;
+			}				
+			return exponent;
+			//System.out.println(number + " equals 10 to the power of " + exponent + ".");	// Comment out in final version.
+			//System.out.println("10^" + exponent + " = " + Math.pow(10, exponent));		// Comment out in final version.
+		}
 	}
 }
