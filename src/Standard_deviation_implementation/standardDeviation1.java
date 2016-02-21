139da0feee2601ab7b33505b1fe980d83d0d7c5a
patde <patdth574@gmail.com>
2016-02-21 13:04:58 -0500

    Update standardDeviation1.java

--- src/Standard_deviation_implementation/standardDeviation1.java
+++ src/Standard_deviation_implementation/standardDeviation1.java
@@ -9,38 +9,39 @@
  * 
  */
 
-public static double calculateStandardDeviation1(double[] inputArray) {
-	double sum = 0;
-	double mean = 0;
-	double valueMinusMean = 0;
-	double sumOfValueMinusMeanSquared = 0;
-	double stdev = 0;
-	double arrayLength = inputArray.length;
-
-	/**
-	 * Calculate mean of entries in array.
-	 */
-	for (int i = 0; i < arrayLength; i++) {
-		sum = sum + inputArray[i];
+public class standardDeviation1 {
+	public static double calculateStandardDeviation1(double[] inputArray) {
+		double sum = 0;
+		double mean = 0;
+		double valueMinusMean = 0;
+		double sumOfValueMinusMeanSquared = 0;
+		double stdev = 0;
+		double arrayLength = inputArray.length;
+	
+		/**
+		 * Calculate mean of entries in array.
+		 */
+		for (int i = 0; i < arrayLength; i++) {
+			sum = sum + inputArray[i];
+		}
+	
+		mean = sum / arrayLength;
+	
+		/**
+		 * Calculate the sum of (entries - mean)^2
+		 */
+		for (int j = 0; j < arrayLength; j++) {
+			valueMinusMean = (inputArray[j] - mean) * (inputArray[j] - mean);
+			sumOfValueMinusMeanSquared = sumOfValueMinusMeanSquared + valueMinusMean;
+		}
+	
+		/**
+		 * Use calculateSqrt2 static method to calculate standard deviation.
+		 */
+		stdev = squareRoot2.calculateSqrt2(sumOfValueMinusMeanSquared/arrayLength);
+	
+		return stdev;
 	}
-
-	mean = sum / arrayLength;
-
-	/**
-	 * Calculate the sum of (entries - mean)^2
-	 */
-	for (int j = 0; j < arrayLength; j++) {
-		valueMinusMean = (inputArray[j] - mean) * (inputArray[j] - mean);
-		sumOfValueMinusMeanSquared = sumOfValueMinusMeanSquared + valueMinusMean;
-	}
-
-	/**
-	 * Use calculateSqrt2 static method to calculate standard deviation.
-	 */
-	stdev = squareRoot2.calculateSqrt2(sumOfValueMinusMeanSquared/arrayLength);
-
-	return stdev;
 }
-
 
 
