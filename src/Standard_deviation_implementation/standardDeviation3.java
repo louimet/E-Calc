7566f0f12c605ba6c5dc6a584b9b036edbb45852
patde <patdth574@gmail.com>
2016-02-22 14:44:39 -0500

    Update standardDeviation3.java

--- src/Standard_deviation_implementation/standardDeviation3.java
+++ src/Standard_deviation_implementation/standardDeviation3.java
@@ -9,35 +9,38 @@
  * where x_i are each of the entries and n is the number of entries.                                   
  */
 
-public class standardDeviation3 {
+public class StandardDeviation3 {
 
-	public static double calculateStandardDeviation3(double[] inputArray) {
+/**
+	 * User inputs values into array x.
+	 */
+	public static double calculate(double[] x) {
 		double entrySquared = 0;
 		double sumOfSquares = 0;
 		double sumOfEntries = 0;
 		double stdev = 0;
-		double arrayLength = inputArray.length;
+		double arrayLength = x.length;
 
 		/**
 		 * Calculate the sum of the squares of each entry in the array.
 		 */
-		for (int i = 0; i < inputArray.length; i++) {
-			entrySquared = inputArray[i] * inputArray[i];
+		for (int i = 0; i < x.length; i++) {
+			entrySquared = x[i] * x[i];
 			sumOfSquares = sumOfSquares + entrySquared;
 		}
 
 		/**
 		 * Calculate the sum of each entry in the array.
 		 */
-		for (int j = 0; j < inputArray.length; j++) {
-			sumOfEntries = sumOfEntries + inputArray[j];
+		for (int j = 0; j < x.length; j++) {
+			sumOfEntries = sumOfEntries + x[j];
 		}
 		sumOfEntries = sumOfEntries * sumOfEntries;
 
 		/**
 		 * Use calculateSqrt2 static method to calculate standard deviation.
 		 */
-		stdev = squareRoot2.calculateSqrt2((1 / (arrayLength * (arrayLength - 1))) * ((arrayLength * sumOfSquares) - sumOfEntries));
+		stdev = squareRoot2.calculate((1 / (arrayLength * (arrayLength - 1))) * ((arrayLength * sumOfSquares) - sumOfEntries));
 
 		return stdev;
 	}
