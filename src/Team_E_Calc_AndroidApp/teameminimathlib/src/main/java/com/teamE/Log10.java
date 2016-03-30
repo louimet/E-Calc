d58063f469fa3628307b8ffa926defacfb7db519
louimet <louimet@aei.ca>
2016-03-30 16:09:18 -0400

    Update Log10.java
    - Now very precise even for very small or very large numbers.
    - Remains to be properly commented

--- src/Team_E_Calc_AndroidApp/teameminimathlib/src/main/java/com/teamE/Log10.java
+++ src/Team_E_Calc_AndroidApp/teameminimathlib/src/main/java/com/teamE/Log10.java
@@ -2,23 +2,26 @@ package com.teamE;
 
 public class Log10 {
 	static public double calculate(double number) {
-		double exponent;
+		double exponent = 0;
 		
 		if(number <= 0) {
 			double badDouble = Double.NaN;
-			return badDouble;
+			exponent = badDouble;
 		} else if(number == 1) {
-			return 0;
-		} else {
-			double temp = number;
-			double floor = 0;
-			for(;temp > 10; floor++) {
-				temp = temp / 10;
+			exponent = 0;
+		} else if (number > 0 && number < 0.1) {
+			for(;number < 0.1; exponent--) {
+				number = number * 10;
 			}
-			temp = number / PowerOfTen.calculate(floor);
-			exponent = floor + (NaturalLog.calculate(temp) / NaturalLog.calculate(10));
-			
-			return exponent;
+			exponent = exponent + (NaturalLog.calculate(number) / NaturalLog.calculate(10));
+		} else if(number >= 0.1 && number < 1) {
+			exponent = NaturalLog.calculate(number) / NaturalLog.calculate(10);
+		} else {
+			for(;number > 1; exponent++) {
+				number = number / 10;
+			}
+			exponent = exponent + (NaturalLog.calculate(number) / NaturalLog.calculate(10));
 		}
+		return exponent;
 	}
 }
