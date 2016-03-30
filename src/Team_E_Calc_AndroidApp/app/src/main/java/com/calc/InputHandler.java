7688506b1ee507dab92f47b148198e47a7fc9233
friketrike <friketrike@gmail.com>
2016-03-30 16:17:26 -0400

    implemented the cursor placement by touching the edit window, cursor is visible and function substrings are atomic. Also disabled the possibility of putting two decimal points to an operand

--- src/Team_E_Calc_AndroidApp/app/src/main/java/com/calc/InputHandler.java
+++ src/Team_E_Calc_AndroidApp/app/src/main/java/com/calc/InputHandler.java
@@ -7,63 +7,60 @@ public class InputHandler {
 
     private static int currIndex = 0;
 
-    public static int getCurrIndex(){
+    public static int getCurrIndex() {
         return currIndex;
     }
 
-    public static void resetCurrIndex(){
+    public static void resetCurrIndex() {
         currIndex = ExpressionHistory.getEntry().length();
     }
 
-    public static void moveLeft(){ // TODO connect a button from the interface (add the button too)
+    public static void setCurrIndex(int newIndex) {
+        final int MAX_NUM_FUN_CHARS = 6;
+        int rightSubExpressionLength = ExpressionHistory.getEntry().length() - newIndex;
+        int rightShift = (MAX_NUM_FUN_CHARS - 1) < rightSubExpressionLength
+                ? MAX_NUM_FUN_CHARS - 1
+                : rightSubExpressionLength;
+        int tempIndex = newIndex + rightShift;
+        for (int i = 0; i < rightShift; i++) {
+            int tempOffset = findLeftOffsetAtIndex(tempIndex);
+            if (tempOffset > tempIndex - newIndex) {
+                newIndex = tempIndex - tempOffset;
+                break;
+            }
+            tempIndex--;
+        }
+        currIndex = newIndex;
+        ExpressionHistory.refreshDisplay = true;
+    }
+
+    public static void moveLeft() { // TODO connect a button from the interface (add the button too)
         if (currIndex == 0)
             return;
-        // we only care about what's left of the cursor
-        String expression = ExpressionHistory.getEntry().substring(0,currIndex);
-        if (expression.matches("(.*)Sin\\($") )
-            currIndex -= 4;
-        else if (expression.matches("(.*)Log10\\($") )
-            currIndex -= 6;
-        else if (expression.matches("(.*)e\\^\\($") )
-            currIndex -= 3;
-        else if (expression.matches("(.*)√\\($") )
-            currIndex -= 2;
-        else if (expression.matches("(.*)10\\^\\($") )
-            currIndex -= 4;
-        else // we should have a single character
-            currIndex -= 1;
+        currIndex -= findLeftOffsetAtIndex(currIndex);
+        ExpressionHistory.refreshDisplay = true;
     }
 
-    public static void moveRight(){ // TODO connect a button from the interface (add the button too)
+    public static void moveRight() { // TODO connect a button from the interface (add the button too)
         if (currIndex == ExpressionHistory.getEntry().length())
             return;
-        // we only care about what's right of the cursor
-        String expression = ExpressionHistory.getEntry().substring(currIndex);
-        if (expression.matches("(.*)Sin\\($") )
-            currIndex += 4;
-        else if (expression.matches("(.*)Log10\\($") )
-            currIndex += 6;
-        else if (expression.matches("(.*)e\\^\\($") )
-            currIndex += 3;
-        else if (expression.matches("(.*)√\\($") )
-            currIndex += 2;
-        else if (expression.matches("(.*)10\\^\\($") )
-            currIndex += 4;
-        else // we should have a single character
-            currIndex += 1;
+        currIndex += findRightOffsetAtIndex(currIndex);
+        ExpressionHistory.refreshDisplay = true;
     }
 
-    public static void moveUp(){
+    public static void moveUp() {
         ExpressionHistory.decrCurrIndex();
+        currIndex = ExpressionHistory.getEntry().length();
     }
 
-    public static void moveDown(){
+    public static void moveDown() {
         ExpressionHistory.incrCurrIndex();
+        currIndex = ExpressionHistory.getEntry().length();
     }
 
-    public static void input(String s){
+    public static void input(String s) {
         int newIndex = currIndex;
-        switch(s){
+        switch (s) {
             case "Sin(x)":
                 s = "Sin(";
                 newIndex += 4;
@@ -80,31 +77,31 @@ public class InputHandler {
                 s = "√(";
                 newIndex += 2;
                 break;
-            case "10x":
-                s = "10^(";
-                newIndex += 4;
-                break;
             case "xy":
                 s = "^";
                 newIndex += 1;
+                break;
+            case ".":
+                s = validateDot();
+                newIndex += s.length();
                 break;
             default:
                 newIndex++;
         }
         String expression = ExpressionHistory.getEntry();
-        String newExpression = expression.substring(0,currIndex)+s+expression.substring(currIndex);
+        String newExpression = expression.substring(0, currIndex) + s + expression.substring(currIndex);
         currIndex = newIndex;// the index in the handler refers to the cursor location, in history to the entry TODO change to more meaningful vars such as cursorLoc and entryIdx
         updateExpressionHistory(newExpression);
     }
 
-    public static void plusMinus(){
+    public static void plusMinus() {
         String expression = ExpressionHistory.getEntry();
         // TODO add appropriate evaluation and modification here, remember to handle currIndex
         // NOTE now we can handle a simple minus sign, no need for a hyphen
         //updateExpressionHistory(newExpression);
     }
 
-    public static void backspace(){
+    public static void backspace() {
         //if (isZS || expression.isEmpty()) return"";
         String expression = ExpressionHistory.getEntry();//TODO this works now with single-display
         String leftSubexpression = expression.substring(0, currIndex);
@@ -113,146 +110,83 @@ public class InputHandler {
             return;
         int newIndex = currIndex;
         int length = leftSubexpression.length();
-        char deleted = leftSubexpression.charAt(length - 1);
-        // checking for functions being removed must go before checking anything else as it will
-        // force us to remove the whole function, leave 1 character so remaining checks and masks work
-        if (leftSubexpression.matches("(.*)Sin\\($") ) {
+        /* checking for functions being removed must go before checking anything else
+        *  as it will forceus to remove the whole function,
+        *  leave 1 character so remaining checks and masks work*/
+        if (leftSubexpression.matches("(.*)Sin\\($")) {
             leftSubexpression = leftSubexpression.substring(0, length - 4);
             newIndex -= 4;
-        }
-        else if (leftSubexpression.matches("(.*)Log10\\($") ) {
+        } else if (leftSubexpression.matches("(.*)Log10\\($")) {
             leftSubexpression = leftSubexpression.substring(0, length - 6);
             newIndex -= 6;
-        }
-        else if (expression.matches("(.*)e\\^\\($") ) {
+        } else if (expression.matches("(.*)e\\^\\($")) {
             leftSubexpression = leftSubexpression.substring(0, length - 3);
             newIndex -= 3;
-        }
-        else if (leftSubexpression.matches("(.*)√\\($") ) {
+        } else if (leftSubexpression.matches("(.*)√\\($")) {
             leftSubexpression = leftSubexpression.substring(0, length - 2);
             newIndex -= 2;
-        }
-        /*else if (leftSubexpression.matches("(.*)10\\^\\($") ) {
-            leftSubexpression = leftSubexpression.substring(0, length - 4);
-            newIndex -= 4;
-        }*/
-        else { // we have a single character
+        } else { // we have a single character
             leftSubexpression = leftSubexpression.substring(0, length - 1);
             newIndex--;
         }
         currIndex = newIndex;
-        updateExpressionHistory(leftSubexpression+rightSubexpression);
+        updateExpressionHistory(leftSubexpression + rightSubexpression);
     }
 
-    public static void clear(){
+    public static void clear() {
         updateExpressionHistory("");
         currIndex = 0;
     }
 
-    static private void updateExpressionHistory(String newExpression){
-        if (ExpressionHistory.getCurrEntry() == ExpressionHistory.getSize()-1){
+    // return the necessary offset to move left and keep function substrings atomic
+    private static int findLeftOffsetAtIndex(int index) {
+        // we only care about what's left of the cursor
+        String expression = ExpressionHistory.getEntry().substring(0, index);
+        if (expression.matches("(.*)Sin\\($"))
+            return 4;
+        else if (expression.matches("(.*)Log10\\($"))
+            return 6;
+        else if (expression.matches("(.*)√\\($"))
+            return 2;
+        else // we should have a single character
+            return 1;
+    }
+
+    // return the necessary offset to move right and keep function substrings atomic
+    private static int findRightOffsetAtIndex(int n) {
+        // we only care about what's right of the cursor
+        String expression = ExpressionHistory.getEntry().substring(n);
+        if (expression.matches("^Sin\\((.*)"))
+            return 4;
+        else if (expression.matches("^Log10\\((.*)"))
+            return 6;
+        else if (expression.matches("^√\\((.*)"))
+            return 2;
+        else // we should have a single character
+            return 1;
+    }
+
+    static private void updateExpressionHistory(String newExpression) {
+        if (ExpressionHistory.getCurrEntry() == ExpressionHistory.getSize() - 1) {
             ExpressionHistory.setEntry(newExpression);
-        }
-        else{ // avoid altering the course of history, modified entries are new entries
+        } else { // avoid altering the course of history, modified entries are new entries
             ExpressionHistory.appendEntry(newExpression);
             currIndex = (ExpressionHistory.getEntry().length());
         }
         ExpressionHistory.refreshDisplay = true;
     }
 
-    /*public static void setWithHistoryEntry(int i){
-        if (i < ExpressionHistory.getSize())
-            ExpressionBuffer.setExpression(ExpressionHistory.getEntry(i));
-            currIndex = ExpressionBuffer.getExpression().length();
-    }*/
+    static private String validateDot(){
+        String subExpression = ExpressionHistory.getEntry().substring(0,currIndex);
+        String s = "";
+        if( subExpression.equals("")
+                || subExpression.matches("(.*(Log10)?\\()$")
+                || subExpression.matches("((.*\\d[^\\.\\d])*\\d*)$")
+                || subExpression.matches("(.*[\\+|-|×|÷|\\^])$") ){
 
-    // This could be of use for the plusMinus modifier key, TODO delete when done
-    /*// ----------BACKSPACE------- This is pretty involved...
-        if (view.getId() == R.id.buttonBackspace) {
-            if (isZS || expression.isEmpty()) return"";
-            int length = expression.length();
-
-
-            char deleted = expression.charAt(length-1);
-            // checking for functions being removed must go before checking anything else as it will
-            // force us to remove the whole function, leave 1 character so remaining checks and masks work
-            if (expression.matches("(.*)Sin\\($") )
-                expression = expression.substring(0, length-3);
-            else if (expression.matches("(.*)Log10\\($") )
-                expression = expression.substring(0, length-5);
-            else if (expression.matches("(.*)e\\^\\($") )
-                expression = expression.substring(0, length-2);
-            else if (expression.matches("(.*)√\\($") )
-                expression = expression.substring(0, length-1);
-            else if (expression.matches("(.*)10\\^\\($") )
-                expression = expression.substring(0, length-3);
-
-            // maybe the length changed, get it again
-            length = expression.length();
-
-            if (length == 1) { // we're done, only the deleted character is there
-                INPUTFLOWMASK = SZALLOW;
-                parenthesisOpen = 0;
-                isZS = true;
-                return "0.0";
-            }
-
-            switch (deleted) {
-                case '(':
-                    parenthesisOpen--;
-                    break;
-                case ')':
-                    parenthesisOpen++;
-                    break;
-                case '.':
-                    INPUTFLOWMASK |= ALLOWDOT;
-                    break;
-                case 'π':
-                    INPUTFLOWMASK |= ALLOWPI;
-
-            }
-
-            char newLast = expression.charAt(length-2);
-
-            if (newLast == '.') INPUTFLOWMASK = ALLOWDIGIT; // only digits after dot
-
-            if (newLast == 'π')
-                INPUTFLOWMASK = ALLOWMODIF | ALLOWLPAR | ALLOWRPAR | ALLOWOPERATOR | ALLOWFUN;
-
-            if (newLast <= '0' && newLast >= '9'){
-                INPUTFLOWMASK |= ALLOWDIGIT | ALLOWDOT | ALLOWPI | ALLOWMODIF | ALLOWLPAR | ALLOWRPAR | ALLOWOPERATOR | ALLOWFUN;
-                int i = 3;
-                while(i > length &&
-                        ( ( expression.charAt(length-i) >= '0' && expression.charAt(length-i) <= '9' ) ||
-                                expression.charAt(length-i) == '.') ) {
-                    if (expression.charAt(length - i) == '.') {
-                        INPUTFLOWMASK &= ~ALLOWDOT;
-                        break;
-                    }
-                }
-            }
-
-            if (newLast == '×' || newLast == '÷' || newLast == '+' )
-                INPUTFLOWMASK = ALLOWOPERAND | ALLOWLPAR | ALLOWFUN;
-
-            if (newLast == '-'){
-                INPUTFLOWMASK = ALLOWOPERAND | ALLOWLPAR | ALLOWFUN;
-                if(length > 2) {
-                    char newNext2Last = expression.charAt(length - 3);
-                    if (newNext2Last == '(')
-                        INPUTFLOWMASK &= ~ALLOWLPAR; // TODO make sure &= is what we need
-                }
-            }
-
-            if (newLast == '(')
-                INPUTFLOWMASK = ALLOWOPERAND | ALLOWLPAR | ALLOWFUN | ALLOWMINUS;
-
-            if (newLast == ')') {
-                INPUTFLOWMASK = ALLOWOPERAND | ALLOWLPAR | ALLOWOPERATOR | ALLOWFUN;
-                if (parenthesisOpen < 0) INPUTFLOWMASK |= ALLOWRPAR;
-            }
-
-            return expression.substring(0, length-1);
+            s = ".";
         }
-        // -------END BACKSPACE------*/
+        return s;
+    }
+
 }
--- src/Team_E_Calc_AndroidApp/app/src/main/java/com/calc/MainActivity.java
+++ src/Team_E_Calc_AndroidApp/app/src/main/java/com/calc/MainActivity.java
@@ -1,20 +1,22 @@
 /**
- * Written by Federico O'Reilly Regueiro for COPM 5541, calculator project
+ * Written by Team-E for COMP 5541, calculator project
  * Winter 2016
  *
  * This class handles the main view of the calculator, it calls InputHandler (controller)
  * for input and ExpressionEvaluator (controller) to handle evaluation
  *
- * Pushing buttons calls interface functions, which has been linked to the appropriate methods in
- * the controllers
+ * Pushing buttons calls the appropriate methods in the controllers
  */
 package com.calc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.os.Vibrator;
@@ -24,7 +26,6 @@ import com.example.friketrin.calc.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO Rad/deg toggle button in menu?
     // TODO Memory, access history et al
     // TODO implement a landscape layout and possibly bigger screen layouts
 
@@ -35,7 +36,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // TODO what do we need to change here?
-        ExpressionHistory.appendEntry(""); // NOTE try to minimize the view calling the model
+        ExpressionHistory.appendEntry(""); // NOTE try to minimize the view calling the mode
+        EditText text = (EditText) findViewById(R.id.textView);
+        text.setOnTouchListener(otl);
+        /* for some reason the xml value doesn't work with galaxy s3 (at least)
+        * so setting this flag ensures that suggestions are off.
+        * http://stackoverflow.com/questions/1959576/turn-off-autosuggest-for-edittext
+        * better this than input of password type (which is a nasty hack) */
+        text.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
     }
 
     @Override
@@ -80,11 +88,26 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+    // Override ontouch to disable soft keyboard and handle cursor position
+    protected View.OnTouchListener otl = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View text, MotionEvent event) {
+            final InputMethodManager imm = ((InputMethodManager) getApplicationContext()
+                    .getSystemService(Context.INPUT_METHOD_SERVICE));
+            try{
+                imm.hideSoftInputFromWindow(text.getApplicationWindowToken(), 0);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            float x = event.getX();
+            float y = event.getY();
+            int touchPosition = ((EditText)text).getOffsetForPosition(x, y);
+            InputHandler.setCurrIndex(touchPosition);
+            populateDisplay();
+            return true;
+        }
+    };
 
-    /* TODO We'll probably need a static int for the current cursor position in the current
-     * position on the expression string and we'll need to pass it along to input handler
-     * this will be done when we have navigation on the text input window
-     */
     // just the messenger, provides an interface between the view and the model
     public void sendMessage(View view){
         Button button = (Button)view;
@@ -99,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void evaluateExpression(View view){//TODO change this once we have two displays (and an ans class)
         String result = ExpressionEvaluator.evaluate();
-        EditText text = (EditText) findViewById(R.id.textView);
+        EditText text = (EditText) findViewById(R.id.textView); //TODO  we should change the id, textView is another type of display
         text.setText(result);
         if(vibrate) {
             Vibrator vibe;
@@ -133,14 +156,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void left(View view){
         InputHandler.moveLeft();
-        EditText text = (EditText) findViewById(R.id.textView);
-        text.setSelection(InputHandler.getCurrIndex());
+        populateDisplay();
     }
 
     public void right(View view){
         InputHandler.moveRight();
-        EditText text = (EditText) findViewById(R.id.textView);
-        text.setSelection(InputHandler.getCurrIndex());
+        populateDisplay();
     }
 
     public void up(View view){
@@ -161,4 +182,5 @@ public class MainActivity extends AppCompatActivity {
             ExpressionHistory.refreshDisplay = false;
         }
     }
+
 }
